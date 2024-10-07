1beb6e28421f0714f663d7bcfdcd24547c2cb2fd
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-10-07 15:02:18 +0000

    added a tempelate dir

--- server.mjs
+++ server.mjs
@@ -1,9 +1,13 @@
 //creating my fisrt server
 import http from 'http'
+import fs from 'fs'
+
+//reading a file 
+const html = fs.readFileSync('./template/hello.html','utf-8')
 
 //creating the server
 const app =  http.createServer((req,res) => {
-    res.end('Hello wORLD')
+    res.end(html)
     console.log('A new request recieved')
 })
 
---
+++ template/scripts/script.js
@@ -0,0 +1,1 @@
+console.alert('hiiiiiiii')
